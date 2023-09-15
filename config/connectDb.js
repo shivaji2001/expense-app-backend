a15c571ac98d62df09300853a5229a3ab8a38982
Shivanshu Singh <shivanshu9569@gmail.com>
2023-09-15 15:33:09 +0530

    first commit

---
+++ config/connectDb.js
@@ -0,0 +1,11 @@
+const { MongoParseError } = require("mongodb");
+const mongoose = require("mongoose");
+const connectDb = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log(`Server running on ${mongoose.connection.host}`.bgCyan.white);
+  } catch (error) {
+    console.log(`${error}`.bgRed);
+  }
+};
+module.exports = connectDb;
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+const colors = require("colors");
+const connectDb = require("./config/connectDB");
+
+dotenv.config();
+
+connectDb();
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/v1/users", require("./routes/userRoute"));
+app.use("/api/v1/transactions", require("./routes/transactionRoutes"));
+const PORT = 8080 || process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`Server running on PORT ${PORT}`);
+});
